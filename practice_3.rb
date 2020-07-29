7e987fff0378edaf7c7d466aaecbb42afc08ca63
Kristofer <kristoferhenriquez@gmail.com>
2020-07-28 21:10:47 -0500

    Exercise 2 done

--- practice_3.rb
+++ practice_3.rb
@@ -1,11 +1,25 @@
 # 1. Write a function that takes in an array of numbers and returns its sum.
 
-array = [2, 5, 7, 8]
+# array = [2, 5, 7, 8]
+# i = 0
+# sum = 0
+# while i < array.length
+#   sum = sum + array[i]
+#   i += 1
+# end
+
+# p sum
+
+# 2. Write a function that takes in an array of strings and returns the smallest string.
+
+array = ["hello", "ok", "kristofer", "world"]
+
+smallest_string = array[0]
 i = 0
-sum = 0
 while i < array.length
-  sum = sum + array[i]
+  if array[i].length < smallest_string.length
+    smallest_string = array[i]
+  end
   i += 1
 end
-
-p sum
+p smallest_string
--- prectice_3.js
+++ prectice_3.js
@@ -1,11 +1,27 @@
 // # 1. Write a function that takes in an array of numbers and returns its sum.
 
-var array = [2, 5, 7, 8];
+// var array = [2, 5, 7, 8];
+// var i = 0;
+// var sum = 0;
+// while (i < array.length) {
+//   sum = sum + array[i];
+//   i++;
+// }
+
+// console.log(sum);
+
+// # 2. Write a function that takes in an array of strings and returns the smallest string.
+
+var array = ["hello", "ok", "kristofer", "world"];
+
+var SmallestString = array[0];
 var i = 0;
-var sum = 0;
+
 while (i < array.length) {
-  sum = sum + array[i];
+  if (array[i].length < SmallestString.length) {
+    SmallestString = array[i];
+  }
   i++;
 }
 
-console.log(sum);
+console.log(SmallestString);
