364bcb4d4f0f258ce612830da608cf999ad3f37e
Kristofer <kristoferhenriquez@gmail.com>
2020-07-28 21:23:03 -0500

    Exercise 3 done

--- practice_3.rb
+++ practice_3.rb
@@ -12,14 +12,44 @@
 
 # 2. Write a function that takes in an array of strings and returns the smallest string.
 
-array = ["hello", "ok", "kristofer", "world"]
+# array = ["hello", "ok", "kristofer", "world"]
 
-smallest_string = array[0]
+# smallest_string = array[0]
+# i = 0
+# while i < array.length
+#   if array[i].length < smallest_string.length
+#     smallest_string = array[i]
+#   end
+#   i += 1
+# end
+# p smallest_string
+
+# 3. Write a function that takes in an array of numbers and returns a new array with the numbers 
+# in reverse order.
+
+# def descending(numbers)
+#   result = []
+#   index = numbers.length - 1
+#   numbers.length.times do
+#     result << numbers[index]
+#     index = index - 1
+#   end
+#   result
+# end
+
+# p descending([9, 2, 1, 3])
+
+# Write a function that takes in an array of words and returns the number of
+#  words that begin with the letter "a".
+
+array = ["hello", "apple", "alan", "banana", "arbol"]
+count = 0
 i = 0
 while i < array.length
-  if array[i].length < smallest_string.length
-    smallest_string = array[i]
+  if array[i][0] == "a"
+    count += 1
   end
-  i += 1
+  i += 1 
 end
-p smallest_string
+
+p count
--- prectice_3.js
+++ prectice_3.js
@@ -12,16 +12,38 @@
 
 // # 2. Write a function that takes in an array of strings and returns the smallest string.
 
-var array = ["hello", "ok", "kristofer", "world"];
+// var array = ["hello", "ok", "kristofer", "world"];
 
-var SmallestString = array[0];
-var i = 0;
+// var SmallestString = array[0];
+// var i = 0;
 
-while (i < array.length) {
-  if (array[i].length < SmallestString.length) {
-    SmallestString = array[i];
-  }
-  i++;
-}
+// while (i < array.length) {
+//   if (array[i].length < SmallestString.length) {
+//     SmallestString = array[i];
+//   }
+//   i++;
+// }
 
-console.log(SmallestString);
+// console.log(SmallestString);
+
+
+// # 3. Write a function that takes in an array of numbers and returns a new array with the numbers 
+// # in reverse order.
+
+
+
+// function Descending(numbers) {
+//   var result = [];
+//   var index = numbers.length - 1;
+//   for (var i = 0; i < numbers.length; i++) {
+//     result.push(numbers[index]);
+//     index = index - 1;
+//   }
+//   return result;
+// }
+
+// console.log(Descending([3, 1, 9, 0]));
+
+
+
+
