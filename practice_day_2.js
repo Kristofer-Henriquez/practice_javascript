ef59faecdd8c6ba3cc162a435ce6937f3f5bed9e
Kristofer <kristoferhenriquez@gmail.com>
2020-07-28 19:49:31 -0500

    Exercise 3 done

--- practice_day_2.js
+++ practice_day_2.js
@@ -15,4 +15,40 @@ function SelectEvenItems(strings) {
   return result;
 }
 
-console.log(SelectEvenItems(["a", "b", "c", "d", "e", "f"]));
+// console.log(SelectEvenItems(["a", "b", "c", "d", "e", "f"]));
+
+
+// # Write a method that accepts one argument - an array of numbers. The method should return the 
+// greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+
+function Max(numbers) {
+  var CurrentMax = numbers[0];
+  numbers.forEach(function(number) {
+    if (number > CurrentMax) {
+      CurrentMax === number;
+    }
+  });
+  return CurrentMax;
+}
+
+// console.log(Max([5, 4, 8, 1, 2]));
+
+
+// # Write a method that accepts a number and returns its factorial. For example, the 
+// factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+
+function Factorial(number) {
+  var result = 1;
+  var CurrentNumber = number;
+  for (var i = 0; i < number; i++) {
+    result = result * CurrentNumber;
+    CurrentNumber = CurrentNumber - 1;
+  }
+  return result;
+}
+
+console.log(Factorial(5));
+
+
